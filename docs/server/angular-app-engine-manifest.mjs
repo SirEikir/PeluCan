041289006e7d94e6beb2225c14d7fce3f7c11e84
SirEikir <cristoferpuerto@gmail.com>
2025-02-14 04:08:57 +0100

    Corrigiendo rutas y subiendo imágenes para GitHub Pages

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/peluqueria-canina',
+  basePath: '/PeluCan',
   supportedLocales: {
   "en-US": ""
 },
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/peluqueria-canina/',
+  baseHref: '/PeluCan/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/peluqueria-canina"
+    "route": "/PeluCan"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5286, hash: 'f9252e02612a071bdd9bbb74b871e9f4ed64bf831351285abcad551220ee1299', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1311, hash: '802f95b8a3d3b3b7560ba6e47a91af92c86b5964e149a48400631da3afdce508', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 17851, hash: '86a29bd20cb404db6bf18643eddaf653c52d3e74e1f69b15f694a080a4f4fc73', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5276, hash: 'b0cbe1cbaed7cb1e2fba6946498a6f3b139425bd8afdce16b28e2dbc923395f8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1301, hash: '6ad2dfe63e62eb598ea366692572fe529db61cb9f523cd48a17ccf46dc2e0e3e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 17841, hash: '2bcd7da4c2c2c723eec0e4552ea1c80fc52cead72191f49ca6a77eb9f579a05d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-VS4E73CL.css': {size: 231889, hash: 'WtbF1+tvvPE', text: () => import('./assets-chunks/styles-VS4E73CL_css.mjs').then(m => m.default)}
   },
 };
